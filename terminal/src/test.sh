8e2b0b1f588e4be277dde5fb87db6b438bec5838
thierry@moncourriel.eu <thierry>
2016-05-04 07:10:57 +0200

    Phase 1
    Ajout d'un petit module de test tmux
    NB : le programme en c ne marche tjrs pas

---
+++ terminal/src/test.sh
@@ -0,0 +1,7 @@
+tmux new -d \; split-window -h -d \; split-window -d
+tmux split-window -t :1.3
+tmux send-keys -t :1.4 "./serveur" ENTER
+for i in 1 2 3; do 
+	tmux send-keys -t :1.$i "cd ../test$i" ENTER
+	tmux send-keys -t :1.$i "./client -n client$i" ENTER
+done
